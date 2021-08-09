77a9348ea57894b688c6d15121c65e880068cd18
jmcarcell <jmcarcell@users.noreply.github.com>
2021-08-09 10:25:41 +0200

    Add some sleep to avoid continuous checking

--- plugins/DQMProcessor.cpp
+++ plugins/DQMProcessor.cpp
@@ -175,6 +175,9 @@ DQMProcessor::RequestMaker()
 
     auto timestamp = m_time_est->get_timestamp_estimate();
     if (timestamp == dfmessages::TypeDefaults::s_invalid_timestamp) {
+      // Some sleep is needed because at the beginning there are no valid timestamps
+      // so it will be checking continuously if there is a valid one
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
       continue;
     }
 
