b5a001f552cf1bd729b913647151e2e194d44041
jmcarcell <jmcarcell@users.noreply.github.com>
2021-10-04 14:27:27 +0200

    Remove save_and_clean
    Maintaining this up to date is too much effort, output can be logged or a simple
    Kafka consumer can be made instead

--- src/HistContainer.hpp
+++ src/HistContainer.hpp
@@ -37,7 +37,6 @@ public:
   void run(dunedaq::dataformats::TriggerRecord& tr, RunningMode mode = RunningMode::kNormal, std::string kafka_address="");
   void transmit(std::string &kafka_address, const std::string& topicname, int run_num, time_t timestamp);
   void clean();
-  void save_and_clean(uint64_t timestamp); // NOLINT(build/unsigned)
 
   int frames_run = 0;
   int max_frames = 1;
@@ -139,21 +138,11 @@ HistContainer::clean()
 }
 
 void
-HistContainer::save_and_clean(uint64_t timestamp) // NOLINT(build/unsigned)
+HistContainer::clean()
 {
-  // TLOG() << "Saving and cleaning histograms";
-
-  std::ofstream file;
-  file.open("Hist/" + m_name + "-" + std::to_string(filename_index) + ".txt", std::ios_base::app);
-  file << timestamp << std::endl;
-
   for (int ich = 0; ich < m_size; ++ich) {
-    file << histvec[ich].m_sum << " ";
+    histvec[ich].clean();
   }
-  file << std::endl;
-  file.close();
-
-  clean();
 }
 
 } // namespace dunedaq::dqm
