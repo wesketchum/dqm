713cf0e3a899d1294db94a08fe2a82d16377d37f
jmcarcell <jmcarcell@users.noreply.github.com>
2021-10-08 13:52:38 +0200

    Change defaults

--- python/dqm/fake_app_confgen.py
+++ python/dqm/fake_app_confgen.py
@@ -167,9 +167,9 @@ def generate(
             ] + [
                 ('dqmprocessor', dqmprocessor.Conf(
                         mode='normal', # normal or debug
-                        sdqm_hist=dqmprocessor.StandardDQM(how_often=60, unavailable_time=1, num_frames=1000),
-                        sdqm_mean_rms=dqmprocessor.StandardDQM(how_often=10, unavailable_time=1, num_frames=1000),
-                        sdqm_fourier=dqmprocessor.StandardDQM(how_often=30, unavailable_time=1, num_frames=1000),
+                        sdqm_hist=dqmprocessor.StandardDQM(how_often=60, unavailable_time=10, num_frames=50),
+                        sdqm_mean_rms=dqmprocessor.StandardDQM(how_often=10, unavailable_time=1, num_frames=100),
+                        sdqm_fourier=dqmprocessor.StandardDQM(how_often=60 * 20, unavailable_time=60, num_frames=100),
                         kafka_address='dqmbroadcast:9092',
                         link_idx=list(range(NUMBER_OF_DATA_PRODUCERS)),
                         clock_frequency=CLOCK_SPEED_HZ
