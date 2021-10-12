91e38c0a8be510f7e4c944553f42c5fd48b611cf
jmcarcell <jmcarcell@users.noreply.github.com>
2021-10-12 15:36:24 +0200

    Remove the debug mode and add a new way of transmitting the info

--- src/FourierContainer.hpp
+++ src/FourierContainer.hpp
@@ -10,6 +10,7 @@
 
 // DQM
 #include "AnalysisModule.hpp"
+#include "ChannelMap.hpp"
 #include "Decoder.hpp"
 #include "Exporter.hpp"
 #include "dqm/Fourier.hpp"
@@ -29,6 +30,7 @@ class FourierContainer : public AnalysisModule
   std::string m_name;
   std::vector<Fourier> fouriervec;
   size_t m_size;
+  int m_npoints;
 
 public:
   FourierContainer(std::string name, int size, double inc, int npoints);
@@ -39,9 +41,10 @@ public:
 
 };
 
-  FourierContainer::FourierContainer(std::string name, int sizedouble, double inc, int npoints)
-  : m_name(name)
-  , m_size(npoints)
+FourierContainer::FourierContainer(std::string name, int size, double inc, int npoints)
+  : m_name(name),
+    m_size(size),
+    m_npoints(npoints)
 {
   for (size_t i = 0; i < m_size; ++i)
     fouriervec.emplace_back(Fourier(inc, npoints));
@@ -50,74 +53,62 @@ public:
 void
 FourierContainer::run(dunedaq::dataformats::TriggerRecord& tr, ChannelMap& map, std::string kafka_address)
 {
+  m_run_mark.store(true);
   dunedaq::dqm::Decoder dec;
   auto wibframes = dec.decode(tr);
-  // std::uint64_t timestamp = 0; // NOLINT(build/unsigned)
-  m_run_mark.store(true);
+  std::uint64_t timestamp = 0; // NOLINT(build/unsigned)
 
-  for (auto fr : wibframes) {
+  for (auto& fr : wibframes) {
 
     for (size_t ich = 0; ich < m_size; ++ich)
       fouriervec[ich].fill(fr->get_channel(ich));
-
-    // Debug mode - save to a file
-    // if (mode != RunningMode::kLocalProcessing) continue;
-    // if (++frames_run == max_frames) // NOLINT(runtime/increment_decrement)
-    // {
-    //   timestamp = fr->get_wib_header()->get_timestamp();
-    //   if (mode == RunningMode::kLocalProcessing) {save_and_clean(timestamp);
-    //   } else if (mode == RunningMode::kNormal) {
-    //     transmit(kafka_address, "testdunedqm", tr.get_header_ref().get_run_number(), tr.get_header_ref().get_trigger_timestamp());
-    //   }
-    //   frames_run = 0;
-    //   ++current_index;
-    //   if (current_index == 2000) {
-    //     ++filename_index;
-    //     current_index = 0;
-    //   }
-    // }
   }
-  // for (size_t ich = 0; ich < m_size; ++ich)
-  //   auto out = fouriervec[ich].compute_fourier_def();
 
-  // Transmit via kafka
-  // if (mode == RunningMode::kNormal){
-  //
-  // }
+  for (size_t ich = 0; ich < m_size; ++ich)
+    fouriervec[ich].compute_fourier_normalized();
+
+  transmit(kafka_address, map, "testdunedqm", tr.get_header_ref().get_run_number(), tr.get_header_ref().get_trigger_timestamp());
 
   m_run_mark.store(false);
-
-  // clean();
 }
 
 void
-FourierContainer::transmit(std::string& kafka_address, const std::string& topicname, int run_num, time_t timestamp)
+FourierContainer::transmit(std::string &kafka_address, ChannelMap& map, const std::string& topicname, int run_num, time_t timestamp)
 {
-  std::stringstream csv_output;
+  // Placeholders
   std::string datasource = "TESTSOURCE";
-  std::string dataname = this->m_name;
-  std::string axislabel = "TESTLABEL";
-  std::stringstream metadata;
-  // metadata << fouriervec[0].m_steps << " " << fouriervec[0].m_low << " " << fouriervec[0].m_high;
-
+  std::string dataname = m_name;
+  std::string metadata = "";
   int subrun = 0;
   int event = 0;
+  std::string partition = "PARTITION";
+  std::string app_name = "APP_NAME";
 
-  // Construct CSV output
-  csv_output << datasource << ";" << dataname << ";" << run_num << ";" << subrun << ";" << event << ";" << timestamp
-             << ";" << metadata.str() << ";";
-  csv_output << axislabel << "\n";
-  for (int ich = 0; ich < 256; ++ich) {
-    csv_output << "Histogram_" << ich + 1 << "\n";
-    // csv_output << fouriervec[ich].m_sum;
-    csv_output << "\n";
+  auto freq = fouriervec[0].get_frequencies();
+  // One message is sent for every plane
+  auto channel_order = map.get_map();
+  for (auto& [key, value] : channel_order) {
+    std::stringstream output;
+    output << datasource << ";" << dataname << ";" << run_num << ";" << subrun
+           << ";" << event << ";" << timestamp << ";" << metadata << ";"
+           << partition << ";" << app_name << ";" << 0 << ";" << key << ";";
+    for (auto& [offch, ch] : value) {
+      output << offch << " ";
+    }
+    output << "\n";
+    for (int i=0; i < freq.size(); ++i) {
+      output << freq[i] << "\n";
+      for (auto& [offch, ch] : value) {
+        output << fouriervec[ch].get_transform(i) << " ";
+      }
+      output << "\n";
+    }
+    TLOG() << "Size of the string: " << output.str().size();
+    // TLOG() << output.str();
+    KafkaExport(kafka_address, output.str(), topicname);
   }
-  // csv_output << "\n";
 
-  // Transmit
-  KafkaExport(kafka_address, csv_output.str(), topicname);
-
-  // clean();
+  clean();
 }
 
 void
