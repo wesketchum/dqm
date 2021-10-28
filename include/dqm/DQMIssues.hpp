6d9dea98fa4da91ca1b098073fdfff990f9b3d25
jmcarcell <jmcarcell@users.noreply.github.com>
2021-10-28 19:40:26 +0200

    Add issues for dqm

---
+++ include/dqm/DQMIssues.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file DQMIssues.hpp DQM system related ERS issues
+ *
+ * This is part of the DUNE DAQ , copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+#ifndef DQM_INCLUDE_DQM_DQMISSUES_HPP_
+#define DQM_INCLUDE_DQM_DQMISSUES_HPP_
+
+#include <ers/Issue.hpp>
+
+#include <string>
+
+namespace dunedaq {
+
+ERS_DECLARE_ISSUE(dqm,
+                  ChannelMapError,
+                  "Channel Map parameter not valid" << error,
+                  ((std::string)error))
+
+ERS_DECLARE_ISSUE(dqm,
+                  InvalidEnvVariable,
+                  "Trying to get the env variable " << env << " did not work",
+                  ((std::string)env))
+
+
+} // namespace dunedaq
+
+#endif // DQM_INCLUDE_DQM_DQMISSUES_HPP_
