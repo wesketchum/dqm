b90cc976fef32a9ebf3a4a3fde1b006c6b34bfa4
jmcarcell <jmcarcell@users.noreply.github.com>
2022-09-19 16:43:04 +0200

    Clean up Hist.hpp

--- include/dqm/Hist.hpp
+++ include/dqm/Hist.hpp
@@ -9,18 +9,7 @@
 #define DQM_INCLUDE_DQM_HIST_HPP_
 
 // DQM
-#include "AnalysisModule.hpp"
-
-#include "daqdataformats/TriggerRecord.hpp"
-
-#include <chrono>
-#include <cmath>
 #include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <ostream>
-#include <string>
 #include <vector>
 
 /**
@@ -38,8 +27,6 @@ class Hist
 public:
   double m_low, m_high, m_step_size;
   int m_nentries = 0;
-  double m_sum = 0, m_sum_sq = 0, m_mean, m_std;
-  bool m_mean_set = false, m_std_set = false;
 
   int m_steps;
   std::vector<int> m_entries;
@@ -58,19 +45,7 @@ public:
    */
   int fill(double x);
 
-  /**
-   * @brief Save to a text file (for debugging purposes)
-   * @param filename Name of the file where the histogram will be saved
-   */
-  void save(const std::string& filename) const;
-
-  /**
-   * @brief Save to a text file (for debugging purposes)
-   * @param filehandle Handle of the file where the histogram will be saved
-   */
-  void save(std::ofstream& filehandle) const;
-
-  bool is_running();
+  bool is_running() const;
   void clean();
 
   double mean();
@@ -108,36 +83,11 @@ Hist::fill(double x)
 
   m_entries[bin]++;
   m_nentries++;
-  m_sum += x;
-  m_sum_sq += x * x;
   return bin;
 }
 
-void
-Hist::save(const std::string& filename) const
-{
-  std::ofstream file;
-  file.open(filename);
-  file << m_steps << " " << m_low << " " << m_high << " " << std::endl;
-  for (auto x : m_entries) {
-    file << x << " ";
-  }
-  file << std::endl;
-  file.close();
-}
-
-void
-Hist::save(std::ofstream& filehandle) const
-{
-  filehandle << m_steps << " " << m_low << " " << m_high << " " << std::endl;
-  for (auto x : m_entries) {
-    filehandle << x << " ";
-  }
-  filehandle << std::endl;
-}
-
 bool
-Hist::is_running()
+Hist::is_running() const
 {
   return true;
 }
@@ -145,35 +95,10 @@ Hist::is_running()
 void
 Hist::clean()
 {
-  m_sum = 0;
-  m_sum_sq = 0;
   m_nentries = 0;
   for (auto& elem : m_entries) {
     elem = 0;
   }
-  m_std_set = false;
-  m_mean_set = false;
-}
-
-double
-Hist::mean()
-{
-  if (m_mean_set)
-    return m_mean;
-  m_mean = m_sum / m_nentries;
-  m_mean_set = true;
-  return m_mean;
-}
-
-double
-Hist::std()
-{
-  if (m_std_set)
-    return m_std;
-  m_mean = mean();
-  m_std = sqrt((m_sum_sq + m_nentries * m_mean * m_mean - 2 * m_sum * m_mean) / (m_nentries - 1));
-  m_std_set = true;
-  return m_std;
 }
 
 } // namespace dunedaq::dqm
