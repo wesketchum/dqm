8fb8b3bd20d1eade7ff88e23321a2b72b08a422f
Eric Flumerfelt <eflumerf@fnal.gov>
2022-09-30 10:50:37 -0500

    Add kProtoWIB to the allowed list of Fragment Types in Decoder. Add abort check if no frames survive the pipeline.

--- src/RMSModule.hpp
+++ src/RMSModule.hpp
@@ -110,6 +110,9 @@ RMSModule::run_(std::unique_ptr<daqdataformats::TriggerRecord> record,
   auto frames = decode<T>(*record, args.max_frames);
   auto pipe = Pipeline<T>({"remove_empty", "check_empty", "make_same_size", "check_timestamp_aligned"});
   pipe(frames);
+  if (frames.size() == 0) {
+    return record;
+  }
 
   // Get all the keys
   std::vector<int> keys;
--- src/STDModule.hpp
+++ src/STDModule.hpp
@@ -111,6 +111,9 @@ STDModule::run_(std::unique_ptr<daqdataformats::TriggerRecord> record,
   auto frames = decode<T>(*record, args.max_frames);
   auto pipe = Pipeline<T>({"remove_empty", "check_empty", "make_same_size", "check_timestamp_aligned"});
   pipe(frames);
+  if (frames.size() == 0) {
+    return record;
+  }
 
   // Get all the keys
   std::vector<int> keys;
